32e88a127b41d4af03c38ff4b768775fd7d9471f
KatsuroAlex <katsuroay@mail.ru>
2023-04-05 00:42:59 +0400

    test

--- frontend/src/utils/Url.js
+++ frontend/src/utils/Url.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://katsuromesto.nomoredomains.monster';
+export const BASE_URL = 'http://api.katsuromesto.nomoredomains.monster';
 // export const BASE_URL = 'http://localhost:3001';
