121018b1c3777facfbe929762589dbd9199cadad
KatsuroAlex <katsuroay@mail.ru>
2023-04-05 13:33:30 +0400

    https certificate enabled

--- frontend/src/utils/Url.js
+++ frontend/src/utils/Url.js
@@ -1,2 +1,3 @@
-export const BASE_URL = 'http://api.katsuromesto.nomoredomains.monster';
+// export const BASE_URL = 'http://api.katsuromesto.nomoredomains.monster';
+export const BASE_URL = 'https://api.katsuromesto.nomoredomains.monster';
 // export const BASE_URL = 'http://localhost:3001';
